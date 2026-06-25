10e86945abce5ce66259265690763fc76203dcbe
RalfHackmann <36924554+RalfHackmann@users.noreply.github.com>
2026-06-25 10:42:53 +0200

    Commit Klasse hinzugefügt

---
+++ src/main/java/de/esw/mavenproject3/NewClass1.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package de.esw.mavenproject3;
+
+/**
+ *
+ * @author U14
+ */
+public class NewClass1 {
+    
+}
